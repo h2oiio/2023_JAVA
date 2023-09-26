b118b9b701d2e39ed1bf6a071e7aba4de278bce1
h2oiio <wcjjcxb@gmail.com>
2023-09-26 11:15:27 +0900

    push 시도

--- Week03_h2o/src/CarPro.java
+++ Week03_h2o/src/CarPro.java
@@ -1,13 +1,107 @@
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-class MyPanel extends JPanel {
-	
-}
-public class CarPro {
+public class CarPro extends JFrame {
+    BufferedImage img = null;
+    int img_x = 75, img_y = 30;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    class MyPanel extends JPanel {
 
-	}
+        public MyPanel() {
+            try {
+                img = ImageIO.read(new File("car.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+            // KeyBindings를 사용하여 키 이벤트 처리
+            this.getInputMap().put(KeyStroke.getKeyStroke("UP"), "moveUp");
+            this.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "moveDown");
+            this.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "moveLeft");
+            this.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "moveRight");
+
+            this.getActionMap().put("moveUp", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    img_y -= 10;
+                    repaint();
+                }
+            });
+
+            this.getActionMap().put("moveDown", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    img_y += 10;
+                    repaint();
+                }
+            });
+
+            this.getActionMap().put("moveLeft", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    img_x -= 10;
+                    repaint();
+                }
+            });
+
+            this.getActionMap().put("moveRight", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    img_x += 10;
+                    repaint();
+                }
+            });
+
+            this.requestFocus();
+            setFocusable(true);
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(img, img_x, img_y, null);
+        }
+    }
+
+    public CarPro() {
+        setSize(350, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new MyPanel());
+        JPanel p = new JPanel();
+        JButton b1 = new JButton("LEFT");
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                img_x -= 10;
+                repaint();
+            }
+        });
+        JButton b2 = new JButton("RIGHT");
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                img_x += 10;
+                repaint();
+            }
+        });
+        p.add(b1);
+        p.add(b2);
+        add(p, "South");
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        CarPro s = new CarPro();
+    }
 }
--- Week03_h2o/src/LoginPage.java
+++ Week03_h2o/src/LoginPage.java
@@ -1,30 +1,95 @@
-import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
 public class LoginPage extends JFrame {
+	
+	private JPanel contentPane;
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
+	private JTextField textField4;
+	private JTextField textField5;
+	private JPasswordField passwordField;
+	private JButton btnNewButton1;
+	private JButton btnNewButton2;
 
 	public LoginPage() {
-		setTitle("회원 등록하기");
-		setSize(300, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 237);
 		
-		JPanel panel = new JPanel();
-		add(panel);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		panel.add(new JLabel("이름"));
-		panel.add(new JTextField(20));
-		panel.add(new JLabel("패스워드"));
-		panel.add(new JPasswordField(20));
+		JLabel lblNewLabel = new JLabel("이름");
+		lblNewLabel.setBounds(36, 45, 151, 15);
+		contentPane.add(lblNewLabel);
 		
-		JButton login = new JButton("등록하기");
-		panel.add(login);
+		JLabel lblNewLabel_1 = new JLabel("패스워드");
+		lblNewLabel_1.setBounds(36, 70, 151, 15);
+		contentPane.add(lblNewLabel_1);
 		
-		JButton cancel = new JButton("cancle");
-		panel.add(cancel);
+		JLabel lblNewLabel_2 = new JLabel("이메일 주소");
+		lblNewLabel_2.setBounds(36, 95, 113, 15);
+		contentPane.add(lblNewLabel_2);
+				
+		JLabel lblNewLabel_3 = new JLabel("전화번호");
+		lblNewLabel_3.setBounds(36, 120, 57, 15);
+		contentPane.add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("회원 등록하기");
+		lblNewLabel_4.setFont(new Font(" ",Font.PLAIN, 16));
+		lblNewLabel_4.setBounds(152, 10, 151, 25);
+		contentPane.add(lblNewLabel_4);
 		
-		setVisible(true);
+		textField1 = new JTextField();
+		textField1.setBounds(187, 45, 116, 21);
+		contentPane.add(textField1);
+		textField1.setColumns(10); //이름 텍스트필드
+				
+		passwordField = new JPasswordField();
+		passwordField.setBounds(187, 70, 116, 21);
+		contentPane.add(passwordField); //패스워드 텍스트필드
+
+		textField2 = new JTextField();
+		textField2.setBounds(187, 95, 116, 21);
+		contentPane.add(textField2);
+		textField2.setColumns(10); //이메일주소 텍스트필드
+
+		textField3 = new JTextField();
+		textField3.setBounds(187, 120, 116, 21);
+		contentPane.add(textField3);
+		textField3.setColumns(10); //이메일주소 텍스트필드
+			
+		btnNewButton1 = new JButton("등록하기");
+		btnNewButton1.setBounds(90, 165, 97, 23);
+		contentPane.add(btnNewButton1);
+		
+		btnNewButton2 = new JButton("취소");
+		btnNewButton2.setBounds(206, 165, 97, 23);
+		contentPane.add(btnNewButton2);
 	}
-}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginPage frame = new LoginPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	}
